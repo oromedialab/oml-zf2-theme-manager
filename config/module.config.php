898f777b25d8a652a76bb02677634f66f65ecdb8
Ibrahim Azhar Armar <azhar@iarmar.com>
2015-09-02 12:14:21 +0530

    Updated module config

--- config/module.config.php
+++ config/module.config.php
@@ -9,28 +9,34 @@
 namespace OmlZf2ThemeManager;
 
 return array(
-    'oml-zf2-theme' => array(
-        'active'  => 'npm-dashboard-default',
+    'view_helpers' => array(
+        'invokables' => array(
+            'omlZf2ThemeManager' => 'OmlZf2ThemeManager\View\Helper\ThemeInfo'
+        )
+    ),
+    'oml-zf2-theme-manager' => array(
+        'active'  => 'bootstrap-default-zf2',
+        'public_directory_path' => __DIR__.'/../../../../public',
         'themes' => array(
             'npm-dashboard-default' => array(
                 'name' => 'NPM Dashboard - Default',
                 'template_path' => __DIR__.'/../themes/npm-dashboard-default',
-                'public_asset_path' => __DIR__.'/../../../../public/themes/npm-dashboard-default'
+                'public_asset_path' => 'themes/npm-dashboard-default/'
             ),
             'npm-dashboard-blue' => array(
                 'name' => 'NPM Dashboard - Blue',
                 'template_path' => __DIR__.'/../themes/npm-dashboard-blue',
-                'public_asset_path' => __DIR__.'/../../../../public/themes/npm-dashboard-blue'
+                'public_asset_path' => 'themes/npm-dashboard-blue/'
             ),
             'npm-dashboard-green' => array(
                 'name' => 'NPM Dashboard - Green',
                 'template_path' => __DIR__.'/../themes/npm-dashboard-green',
-                'public_asset_path' => __DIR__.'/../../../../public/themes/npm-dashboard-green'
+                'public_asset_path' => 'themes/npm-dashboard-green/'
             ),
             'bootstrap-default-zf2' => array(
                 'name' => 'Default Bootstrap Theme for ZF2',
                 'template_path' => __DIR__.'/../themes/bootstrap-default-zf2',
-                'public_asset_path' => __DIR__.'/../../../../public/themes/bootstrap-default-zf2'
+                'public_asset_path' => 'themes/bootstrap-default-zf2/'
             )
         )
     )
